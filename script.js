ce09883cdaebcadbd7766b8c4f7c191d3d196b09
Anna Cho <loveablessing@gmail.com>
2022-05-08 14:26:13 -0400

    added randomizer button for different pictures

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+var husbandoArray = new Array("https://www.pockettactics.com/wp-content/uploads/2022/03/genshin-impact-ayato.jpg", "https://cdn.oneesports.gg/cdn-data/2022/01/GenshinImpact_Zhongli_drinking_tea.webp", "https://cdn.mos.cms.futurecdn.net/JCmb5Spk5iTnpqiDbMUsXN.jpg")
+
+const button = document.getElementById("Randombutton");
+button.addEventListener('click', function() {
+    var randomNum = Math.floor(Math.random() * husbandoArray.length);
+    document.getElementById("newHusbando").src = husbandoArray[randomNum];
+});
